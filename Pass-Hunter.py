8544b8ff5bc731a55e92158d2091760709c4e8d6
Jean <kripto@pop-os.localdomain>
2020-10-28 18:10:54 -0400

    PWDB 2

---
+++ Pass-Hunter.py
@@ -0,0 +1,46 @@
+import requests
+import json
+
+
+
+torport = 9050
+proxies = {
+    'http': "socks5h://localhost:{}".format(torport),
+    'https': "socks5h://localhost:{}".format(torport)
+}
+
+
+banner = '''
+         ___  ____ ____ ____    _  _ _  _ _  _ ___ ____ ____ 
+         |__] |__| [__  [__  __ |__| |  | |\ |  |  |___ |__/ 
+         |    |  | ___] ___]    |  | |__| | \|  |  |___ |  \ 
+                                                             
+ '''
+
+print(banner)
+
+
+print("Exemplo de uso email: teste@gmail.com")
+print("User: teste")
+print("Dominio: gmail.com")
+
+print("\n")
+username = input("Digite o user: ")
+domain = input("Digite o dominio: ")
+print("Procurando...")
+
+
+
+request_data = {'luser': username, 'domain': domain, 'luseropr': 1, 'domainopr': 1, 'submitform': 'em'.format(username, domain)}
+url = "http://pwndb2am4tzkvold.onion"
+r = requests.post(url, proxies=proxies, data=request_data)
+string = r.text
+
+start = string.find('<pre>')
+finish = string.find('</pre>')
+cos = string[start:finish]
+
+print(cos)
+        
+# O output n ta arrumado meu pc desligou e eu perdi tudo 
+# N to afim de arrumar isso agora :)
---
+++ install.sh
@@ -0,0 +1,5 @@
+pip3 install requests
+pip3 install requests[socks]
+apt-get install build-essential libssl-dev libffi-dev python-dev
+pip3 install requests[security]
+
